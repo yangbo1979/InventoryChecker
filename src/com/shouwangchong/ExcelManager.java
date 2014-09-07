9f7854008a0382247c89df237fa26373cb5c29c9
yangbo1979 <gyangbo@gmail.com>
2014-09-07 23:30:24 +0800

    update ,fixed save problem.

--- src/com/shouwangchong/ExcelManager.java
+++ src/com/shouwangchong/ExcelManager.java
@@ -12,10 +12,13 @@ import java.util.Date;
 import jxl.CellType;
 import jxl.Workbook;
 import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
 public class ExcelManager
 {
 	private String filePath;
+	private String sheet1Name="sheet1";
 	private int rows;
 	private int cols;
 	private jxl.Workbook rw;
@@ -55,9 +58,11 @@ public class ExcelManager
 			InputStream is = new FileInputStream(filePath);
 			// 创建只读的 Excel 工作薄的对象.
 			rw = jxl.Workbook.getWorkbook(is);
+			sheet1Name = rw.getSheet(0).getName();
 			// 创建可写入的 Excel 工作薄对象
 			wwb = Workbook.createWorkbook(new File(filePath), rw);
-			
+
+			//wwb = Workbook.createWorkbook(new File(outputFilePath), rw);
 			// 读取第一张工作表
 			ws = wwb.getSheet(0);
 			rows = ws.getRows();
@@ -237,13 +242,51 @@ public class ExcelManager
 
 	private void writeFile()
 	{
+		
+		
+		try {  
+            // 创建或打开Excel文件  
+            WritableWorkbook book = Workbook.createWorkbook(new File(  
+            		filePath));  
+  
+            // 生成名为“第一页”的工作表,参数0表示这是第一页  
+            WritableSheet sheet1 = book.createSheet(sheet1Name, 0);  
+//            WritableSheet sheet2 = book.createSheet("第三页", 2);  
+  
+            // 在Label对象的构造函数中,元格位置是第一列第一行(0,0)以及单元格内容为test  
+            int r = getRows();
+    		int c = getCols();
+    		for(int i = 0 ;i<r;i++)
+    		{
+    			for(int j = 0;j<c+1;j++)
+    			{
+    	            Label label = new Label(j, i, getCell(i,j));  
+    				System.out.print(getCell(i,j)+",");
+		            // 将定义好的单元格添加到工作表中  
+		            sheet1.addCell(label);  
+    			}
+    			System.out.println();
+    		}
+  
+            /* 
+             * 生成一个保存数字的单元格.必须使用Number的完整包路径,否则有语法歧义 
+             */  
+//            jxl.write.Number number = new jxl.write.Number(1, 0, 555.12541);  
+//            sheet2.addCell(number);  
+  
+            // 写入数据并关闭文件  
+            book.write();  
+            book.close();  
+        } catch (Exception e) {  
+            System.out.println(e);  
+        }  
 		// 写入 Excel 对象
-		try {
-			wwb.write();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			wwb.write();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 
 	public boolean closeFile()
